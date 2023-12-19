f81388699f8614229479f15f1339f3b1b3016325
Vernon Hudson <vhudson@crimson.ua.edu>
2023-12-19 15:33:17 -0600

    Need to add features

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Create a new public class named BankAccount
+//In your BankAccount class, create the following:
+//A private field of type double named **balance **with a value of 0
+//Define a method named Deposit that will accept a double and store that value in the balance field
+//Define a method named GetBalance that will return the amount stored in the balance field
+//In the main method of your application, create a new instance of the BankAccount class. Allow the user of the application to Deposit money and retrieve their balance through the console.
+//Once finished save, commit, and push back to Github
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0; //private field
+
+        public void Deposit(double balance) //Deposit Method - constructor to assign balance property
+        {
+            Console.WriteLine($"A deposit of {balance,0:c} has been made to your account.");
+            _balance = balance;
+        }
+
+        public double GetBalance() //GetBalance Method
+        {
+            return _balance;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount ba = new BankAccount(); //instance of class Bank account
+            Console.WriteLine($"Welcome to HAHIR Capital. How much do you want to deposit?");
+
+            double amountToDeposit = double.Parse(Console.ReadLine()); //user unput for amount to be accepted as a double
+
+            ba.Deposit(amountToDeposit);//call of Deposit method to create new deposit
+
+            double accountBalance = ba.GetBalance(); //output of Getbalance method to create new account balance
+
+            Console.WriteLine($"Daily Available Balance: {accountBalance, 0:c}");
         }
     }
 }
